d0c053e69890f80a5b163c383a16451e4d48fb15
appsdata_user003 <titus@fibrcorp.io>
2024-11-01 23:53:43 +0700

    add: name

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ const APP_PORT = 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-  const { id } = req.query;
-  res.status(200).send({ id });
+  const { id, name } = req.query;
+  res.status(200).send({ id, name });
 });
 
 app.listen(APP_PORT, () => {
